20bfe00d1577cacbbddbc9e0c80f120024a0a94a
Stefan Toman <git@stoman.de>
2018-02-21 21:37:37 +0100

    finish scoring

--- pizza/code/part2.cpp
+++ pizza/code/part2.cpp
@@ -2,6 +2,7 @@
 #include "util.cpp"
 
 vector<pair<int, int>> slices;
+vector<pair<int, int>> neighbors = {make_pair(-1, 0), make_pair(1, 0), make_pair(0, -1), make_pair(0, 1)};
 
 int score(pair<int, int> &field, Input &input) {
 	//set up slices
@@ -14,9 +15,12 @@ int score(pair<int, int> &field, Input &input) {
 			}
 		}
 	}
+
 	//validate
 	if(field.first < 0 || field.first >= input.r) return -1;
 	if(field.second < 0 || field.second >= input.c) return -1;
+
+	//try slices
 	int max = -1;
 	for(auto &slice: slices) {
 		if(field.first + slice.first - 1 >= input.r) break;
@@ -26,10 +30,19 @@ int score(pair<int, int> &field, Input &input) {
 		for(int i = field.first; i < field.first + slice.first; i++) {
 			for(int j = field.second; j < field.second + slice.second; j++) {
 				if(input.sliced[i][j]) valid = false;
+				for(auto &neigh: neighbors) {
+					if(0 <= i + neigh.first && i + neigh.first < input.r && 0 <= j + neigh.second && j + neigh.second < input.c) {
+						if(input.sliced[i + neigh.first][j + neigh.second]) {
+							s++;
+						}
+					}
+				}
 			}
 		}
 		if(valid && s > max) max = s;
 	}
+
+	//return
 	return max;
 }
 
