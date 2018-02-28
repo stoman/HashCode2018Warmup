952235984d6bf3b84ada20f6d435bc88018553dd
Christian Müller <christian.mueller@tum.de>
2018-02-28 21:17:27 +0100

    example

--- replyways/visualize/plot.sh
+++ replyways/visualize/plot.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 FILENAME1=$1
-FILENAME2=$2
+# FILENAME2=$2
 
-gnuplot -e "datafile='${FILENAME1}';datafile2='${FILENAME2}'; outputname='${FILENAME1}.png'" plot.plg
+# gnuplot -e "datafile='${FILENAME1}';datafile2='${FILENAME2}'; outputname='${FILENAME1}.png'" plot.plg
+
+gnuplot -e "datafile='${FILENAME1}'; outputname='${FILENAME1}.png'" plot_input.plg
